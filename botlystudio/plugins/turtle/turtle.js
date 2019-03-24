9f2dd4090b25d3ad6fec3bcf88f1f36888672442
Jules Topart <jules.topart@gmail.com>
2019-03-24 21:38:19 +0100

    Update turtle.js

--- botlystudio/plugins/turtle/turtle.js
+++ botlystudio/plugins/turtle/turtle.js
@@ -998,7 +998,7 @@ Turtle.executeChunk_ = function() {
       go = false;
       if(Turtle.fastMode){
         Turtle.pidList.push(
-          setTimeout(Turtle.executeChunk_, 1));
+          setTimeout(Turtle.executeChunk_, 0.01));
       }else{
         Turtle.pidList.push(
           setTimeout(Turtle.executeChunk_, Turtle.pause));
